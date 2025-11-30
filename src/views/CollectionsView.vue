bdd540a12adfe849165bff07f2a6157b61b54071
Carlos <carlosjperazab@gmail.com>
2025-11-30 19:10:02 -0400

    Movimos las imagenes de modelo a la carpeta assets

--- src/views/CollectionsView.vue
+++ src/views/CollectionsView.vue
@@ -2,6 +2,17 @@
 import HeaderPagina from "@/views/components/header/HeaderPagina.vue";
 import Section from "@/components/padres/Section.vue";
 import Img from "@/components/padres/Img.vue";
+
+import img1 from "@/assets/img/vistas/active-states-basket-empty.jpg";
+import img2 from "@/assets/img/vistas/active-states-basket-filled.jpg";
+import img3 from "@/assets/img/vistas/active-states-lightbox.jpg";
+import img4 from "@/assets/img/vistas/desktop-design-lightbox.jpg";
+import img5 from "@/assets/img/vistas/desktop-design.jpg";
+import img6 from "@/assets/img/vistas/desktop-preview.jpg";
+import img7 from "@/assets/img/vistas/mobile-design-basket-empty.jpg";
+import img8 from "@/assets/img/vistas/mobile-design-basket-filled.jpg";
+import img9 from "@/assets/img/vistas/mobile-design.jpg";
+import img10 from "@/assets/img/vistas/mobile-menu.jpg";
 </script>
 
 <template>
@@ -11,71 +22,62 @@ import Img from "@/components/padres/Img.vue";
 
     <Section class="flex flex-col gap-4 items-center">
       <Img
-        src="../../public/images/vistas/active-states-basket-empty.jpg"
-        alt="Foto de perfil"
+        :src="img1"
+        alt="Basket vacío"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/active-states-basket-filled.jpg"
-        alt="Ejemplo remoto"
+        :src="img2"
+        alt="Basket lleno"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/active-states-lightbox.jpg"
-        alt="Ejemplo remoto"
+        :src="img3"
+        alt="Lightbox activo"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/desktop-design-lightbox.jpg"
-        alt="Ejemplo remoto"
+        :src="img4"
+        alt="Lightbox desktop"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/desktop-design.jpg"
-        alt="Ejemplo remoto"
+        :src="img5"
+        alt="Diseño desktop"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/desktop-preview.jpg"
-        alt="Ejemplo remoto"
+        :src="img6"
+        alt="Preview desktop"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/mobile-design-basket-empty.jpg"
-        alt="Ejemplo remoto"
+        :src="img7"
+        alt="Basket vacío móvil"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/mobile-design-basket-filled.jpg"
-        alt="Ejemplo remoto"
+        :src="img8"
+        alt="Basket lleno móvil"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/mobile-design.jpg"
-        alt="Ejemplo remoto"
+        :src="img9"
+        alt="Diseño móvil"
         class="w-full rounded-md shadow-md border"
         lazy
       />
-
       <Img
-        src="../../public/images/vistas/mobile-menu.jpg"
-        alt="Ejemplo remoto"
+        :src="img10"
+        alt="Menú móvil"
         class="w-full rounded-md shadow-md border"
         lazy
       />
